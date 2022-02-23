9be64f3a572a5d91da2bea91080c1c3924e9aa92
Amine8128 <lm_bouchoucha@esi.dz>
2022-02-24 00:50:35 +0100

    initial commit

---
+++ Untitled1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a = 0 ;
+int main()
+{
+  printf("enter your name pls");
+  string name = get_string("What's your name ?\n");
+  printf("%s", name);
+}
+
